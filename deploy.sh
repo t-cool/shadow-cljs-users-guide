1050ea38cf386ae18702414f709ef900b23cbafa
t-cool <hiroki802@hotmail.com>
2021-04-03 07:53:02 +0900

    デプロイの自動化

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
+make
+
 git add .
 
-git diff --name-only origin/master | perl -pe 's/\n/ /'| read -d '' x
+git diff --name-only origin/master | perl -pe 's/\n/ /'| pbcopy
 
-commit_message="update "$x
+commit_message="update: $(pbpaste)"
 
 git commit -m $commit_message
 
 git push -u origin master
-
 
