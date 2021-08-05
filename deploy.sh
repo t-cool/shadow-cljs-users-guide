26bbcb5c8433e6694ac5d27d081260fe24c5d023
t-cool <hiroki802@hotmail.com>
2021-08-05 15:22:45 +0900

    updated: deploy.sh docs/UsersGuide.adoc docs/UsersGuide.html docs/UsersGuide.pdf docs/build-config.adoc docs/default-theme.yml docs/editor-integration.adoc docs/failed-to-load.adoc docs/intro.adoc docs/js-deps.adoc docs/maven-publish.adoc docs/release.adoc docs/repl-troubleshoot.adoc docs/repl.adoc docs/target-browser.adoc docs/target-node-library.adoc docs/target-react-native.adoc docs/usage.adoc

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 bundle exec asciidoctor -o docs/UsersGuide.html -b html5 -r asciidoctor-diagram docs/UsersGuide.adoc
 
-# asciidoctor-pdf -a scripts=cjk -a pdf-theme=default-with-fallback-font -r asciidoctor-diagram docs/UsersGuide.adoc
+asciidoctor-pdf -a scripts=cjk -a pdf-theme=default-with-fallback-font -r asciidoctor-diagram docs/UsersGuide.adoc
 
 git add .
 
